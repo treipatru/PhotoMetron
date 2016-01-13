481aeb2f6b8aa25a2dad9c625832eed6473a1553
Andrei34 <andrei@planet34.org>
2016-01-13 21:12:13 +0200

    V 0.8
    First commit with mostly everything working.
    Needs fix > transform sdCardFile to working datatype

---
+++ memotronic.ino
@@ -0,0 +1,165 @@
+//-----------------------------------------------------------------------------
+// Initialize Things
+//-----------------------------------------------------------------------------
+
+
+//*****************************************************************************
+//***** Printer
+#include "SoftwareSerial.h"                 // Serial comms library
+#include "Adafruit_Thermal.h"               // Printer library
+#define TX_PIN 6 // Arduino transmit  YELLOW WIRE  labeled RX on printer
+#define RX_PIN 5 // Arduino receive   GREEN WIRE   labeled TX on printer
+SoftwareSerial mySerial(RX_PIN, TX_PIN);    // Declare SoftwareSerial obj first
+Adafruit_Thermal printer(&mySerial);        // Pass addr to printer constructor
+
+
+
+
+
+//*****************************************************************************
+//***** Card Reader
+#include <SPI.h>                            // SPI library
+#include <SD.h>                             // SD card SPI library
+const int sdPin = 8;                        // SD card reader pin
+
+
+//*****************************************************************************
+//***** Set Variables
+unsigned long     systemBoot;               // Seconds since system boot
+unsigned long     lastPrint;                // Seconds since last print
+const int         printInterval = 5;        // Minimum seconds between prints
+boolean           readyForPrint;            // If true, buttonpress will print
+
+const int         buttonPin = 2;            // Connected button
+const int         ledPin = 10;              // Led output
+int               pinStatus = 0;            // Read pin status
+
+const int         printWidth = 384;         //Width of images
+const int         printHeight = 500;        //Height of images
+
+//-----------------------------------------------------------------------------
+// SETUP
+//-----------------------------------------------------------------------------
+void setup() {
+
+
+  //***************************************************************************
+  //***** Button Configuration
+  pinMode (ledPin, OUTPUT);
+  pinMode (buttonPin, INPUT_PULLUP);
+
+
+  //***************************************************************************
+  //***** Printer
+  mySerial.begin(19200);                    // Init software serial
+  printer.begin();                          // Init printer
+  lastPrint = 0;
+
+
+  //***************************************************************************
+  //***** Card Reader
+  pinMode (sdPin, OUTPUT);                  // Init card reader
+
+  if (!SD.begin(sdPin)) {                   // Notify if card has issues
+    printer.doubleHeightOn();
+    printer.println(F("OH NOES!!!"));
+    printer.doubleHeightOff();
+    printer.println ("SD can't be read. Maybe ...");
+    printer.feed(1);
+    printer.println ("... the card is missing?");
+    printer.println ("... the reader is damaged?");
+    printer.feed(2);
+    fResetPrinter();
+    return;
+  }
+  printer.feed(2);
+
+  randomSeed(analogRead(0));                // Unused analog as seed
+
+}
+
+
+
+
+
+//-----------------------------------------------------------------------------
+// LOOP
+//-----------------------------------------------------------------------------
+void loop() {
+
+
+  //***************************************************************************
+  //***** Timer Managament
+  systemBoot = millis() / 1000;
+
+
+  if ((systemBoot - lastPrint) < printInterval) { // Seconds until print allowed
+    readyForPrint = false;
+    digitalWrite(ledPin, LOW);
+  } else {
+    readyForPrint = true;
+    digitalWrite(ledPin, HIGH);
+  }
+
+  //***************************************************************************
+  //***** Button Management
+  fCheckButton ();
+}
+
+
+
+
+
+//-----------------------------------------------------------------------------
+// OTHER
+//-----------------------------------------------------------------------------
+
+
+//***************************************************************************
+//***** Button Press Function
+int fCheckButton () {
+  pinStatus = digitalRead(buttonPin);         // Read input value
+
+  if (pinStatus == HIGH) {
+  } else {
+    if ( readyForPrint == true) {
+      fPrintFile ();
+    }
+  }
+
+}
+
+
+//***************************************************************************
+//***** Print a file
+int fPrintFile () {
+
+  int randomNumber = random (16);
+  String fileExt = ".bin";
+  String sdCardFileName;
+
+  sdCardFileName = randomNumber + fileExt;
+
+  File sdCardFile = SD.open (sdCardFileName, FILE_READ);
+
+  while (sdCardFile.available()) {
+    printer.printBitmap(printWidth, printHeight, dynamic_cast<Stream*>(&sdCardFile));
+  }
+  sdCardFile.close();
+
+  delay(1000);                                  // 1 sec delay
+
+  printer.feed(2);                              // Breathing room
+  lastPrint = millis() / 1000;                  // Reset print timer
+  fResetPrinter();                              // Reset printer
+}
+
+
+//***************************************************************************
+//***** Reset the printer
+int fResetPrinter () {
+  printer.sleep();                              // Go to sleep
+  delay(3000L);                                 // Wait 3 seconds
+  printer.wake();                               // wake() before new print
+  printer.setDefault();                         // Restore defaults
+}
